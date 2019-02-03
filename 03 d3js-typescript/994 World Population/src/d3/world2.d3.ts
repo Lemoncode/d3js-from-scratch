280266376040d2ead1ab01104ec340e3177bde6e
Javier Calzado <fjcalzadorodriguez@gmail.com>
2019-02-03 21:32:17 +0100

    Added world population and birth rate animated example

--- 03 d3js-typescript/994 World Population/src/d3/world2.d3.ts
+++ 03 d3js-typescript/994 World Population/src/d3/world2.d3.ts
@@ -1,10 +1,12 @@
-import { mouse } from "d3";
+import "d3-transition";
 import { select } from "d3-selection";
 import { geoNaturalEarth1, geoPath } from "d3-geo";
-import { interpolateBlues } from "d3-scale-chromatic";
-import { scaleSqrt } from "d3-scale";
+import { interpolateGnBu, interpolateOrRd } from "d3-scale-chromatic";
+import { scaleLinear, scaleSqrt } from "d3-scale";
 import { json } from "d3-fetch";
 import { FeatureCollection, Geometry, Feature } from 'geojson';
+import { isNumber } from 'util';
+
 
 // (*) Lets define a width and height for the SVG user space
 // coordinate system (viewBox), also a padding to avoid
@@ -51,17 +53,19 @@ const pathCreator = geoPath()
   .projection(projection);
 
 // (*) Lets implement a useful scale to assign color to
-// countries based on its population.
-const populationLogScale = scaleSqrt().exponent(1/6)
-  .domain([0, 1500000000])
-  .range([0, 1]);
-const colorScale = (population: number) => population ?
-  interpolateBlues(populationLogScale(population)) : "pink";
+// countries based on its population grow.
+const popIncreaseScale = scaleSqrt().exponent(1/2).domain([0, 0.2]).range([0, 1]);
+const popDecreaseScale = scaleSqrt().exponent(1/4).domain([-0.1, 0]).range([1, 0]);
+const colorScale = (delta: number) => isNumber(delta) ?
+  delta >= 0 ? 
+    interpolateGnBu(popIncreaseScale(delta)) :
+    interpolateOrRd(popDecreaseScale(delta)) :
+  "pink";
 
 // (*) Let's load the needed data. This time we will use an
 // async helper borrowed from Promise. We are requiring local
 // files using require() for webpack to provide us with the right
-// path. 
+// path.
 Promise.all([
   json(require("../data/countries.geojson")),
   json(require("../data/population.stats.json"))
@@ -80,32 +84,47 @@ interface PopulationRecord {
   "Country Name": string;
   "Value": number;
   "Year": number;
+  "Delta": number;
 }
 
 // Here is the function.
 function startChart([countries, population]: [FeatureCollection<Geometry, CountryProps>, PopulationRecord[]]) {
-  // Utility function to lookup population by year and country.
-  const populationLookupFunction = createPopulationLookup(population);
-  
+  // Compute delta population as the population grow percentage.
+  calculateDeltaPopulation(population);
+
+  // Utility function to lookup delta population by year and country.
+  const populationDeltaLookupFunction = createDeltaPopulationLookup(population);
+
   // First paint, just countries with no population data.
   enterPattern(countries);
-
+  
   // Now, lets simulate population data is changing over time.
   // 1 year = 1 second.
   const totalYears = 56;
   let yearIndex = 0;
   setInterval(() => {
     const year = 1960 + yearIndex++ % totalYears;
-    updatePattern(year, countries, populationLookupFunction);
+    // And update our visualization accordingly.
+    updatePattern(year, countries, populationDeltaLookupFunction);
   }, 1000);
-}
+};
 
-const createPopulationLookup = (population: PopulationRecord[]) =>
+const calculateDeltaPopulation = (population: PopulationRecord[]) => {
+  population.forEach(record => {
+    const previousRecord = population.find(r =>
+      r["Country Name"] === record["Country Name"] && r["Year"] === record["Year"] - 1);
+    record["Delta"] = previousRecord ?
+      (record["Value"] - previousRecord["Value"]) / previousRecord["Value"] 
+      : 0;
+  });
+};
+
+const createDeltaPopulationLookup = (population: PopulationRecord[]) =>
   (year: number, countryName: string) => {
     const record = population.find(record =>
       record["Country Name"] === countryName && record["Year"] === year);
-    return record ? record["Value"] : undefined;
-}
+    return record ? record["Delta"] : 0;
+};
 
 const enterPattern = (countries: FeatureCollection<Geometry, CountryProps>) => {
   countriesGroup.selectAll("path")
@@ -113,7 +132,7 @@ const enterPattern = (countries: FeatureCollection<Geometry, CountryProps>) => {
     .enter()
     .append("path")
       .attr("d", pathCreator)
-      .attr("fill", d => colorScale(d.properties.population))
+      .attr("fill", d => colorScale(0))
       .style("stroke", "white")
       .style("stroke-width", "0.5px");
 }
@@ -121,11 +140,13 @@ const enterPattern = (countries: FeatureCollection<Geometry, CountryProps>) => {
 const updatePattern = (
   year: number,
   countries: FeatureCollection<Geometry, CountryProps>,
-  getPopulation: (year: number, countryName: string) => number,
+  getDeltaPopulation: (year: number, countryName: string) => number,
 ) => {
   mainTitle.text(`Year ${year.toLocaleString()}`);
 
   countriesGroup.selectAll("path")
     .data(countries.features, (d: Feature<Geometry, CountryProps>) => d.properties.name)
-      .attr("fill", d => colorScale(getPopulation(year, d.properties.name)))
+      .transition()
+      .duration(900)
+        .attr("fill", d => colorScale(getDeltaPopulation(year, d.properties.name)))
 }
